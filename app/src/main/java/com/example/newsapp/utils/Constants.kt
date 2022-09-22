6dbff1d58353ddf5d7d5281abc05d707cedaa00d
Kiddos <darwayl17@gmail.com>
2022-09-23 00:21:10 +0300

    Commit three

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val  BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "b775a6f033504d63a3ea9e84ed1422f8"
+
+    }
+}
